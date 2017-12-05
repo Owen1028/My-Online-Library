10358046dbd01cc2c5b3f43d9f3d5d99ee484095
Owen1028 <owene102827@gmail.com>
2017-12-05 18:25:21 -0500

    Add files via upload

---
+++ MyonlineLibrary/inc/data.php
@@ -0,0 +1,138 @@
+<?php
+$catalog = [];
+
+//Books
+$catalog[101] = [
+	"title" => "Black Privilege",
+	"img" => "img/media/black privilege.jpg",
+    "genre" => "Self-help/ Biography",
+    "format" => "Paperback",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "Charlamagne Tha God"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+
+$catalog[102] = [
+	"title" => "The Subtle Art of Not Giving a F*ck",
+	"img" => "img/media/subtle art.jpg",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 2016,
+    "category" => "Books",
+    "authors" => [
+        "Mark Manson"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+
+$catalog[103] = [
+	"title" => "The Alchemist",
+	"img" => "img/media/the alchemist.jpg",
+    "genre" => "Fiction/ Adventure",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "Paulo Coelho"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+
+$catalog[104] = [
+	"title" => "The Catcher in the Rye",
+	"img" => "img/media/catcher.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "J.D Salinger"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+
+
+//Movies
+$catalog[201] = [
+    "title" => "Friday",
+    "img" => "img/media/friday.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 1995,
+    "category" => "Movies",
+    "director" => "F. Gary Gray",
+    "writers" => [
+        "Ice Cube, DJ Pooh"
+    ],
+    "stars" => [
+        "Ice Cube"
+    ]
+];
+$catalog[202] = [
+    "title" => "Pulp Fiction",
+    "img" => "img/media/pulpfic.jpg",
+    "genre" => "Crime",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Quentin Tarantino",
+    "writers" => [
+        "Quentin Tarantino, Roger Avary"
+    ],
+    "stars" => [
+        "Samuel L. Jackson"
+    ]
+];
+$catalog[203] = [
+    "title" => "Pokemon",
+    "img" => "img/media/pokemon.jpg",
+    "genre" => "Fantasy/ Adventure",
+    "format" => "DVD",
+    "year" => 1998,
+    "category" => "Movies",
+    "director" => "Kunihiko Yuyama, Michael Haigney",
+    "writers" => [
+        "Satoshi Tajiri, Takeshi Shudo"
+    ],
+    "stars" => [
+
+    ]
+];
+
+
+//Music
+$catalog[301] = [
+    "title" => "Perfect Timing",
+    "img" => "img/media/NAV.jpg",
+    "genre" => "Hip-hop",
+    "format" => "mp3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "NAV"
+];
+$catalog[302] = [
+    "title" => "Big Fish Theory",
+    "img" => "img/media/Big fish.jpg",
+    "genre" => "Hip-hop",
+    "format" => "mp3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Vince Staples"
+];
+$catalog[303] = [
+    "title" => "#Santaanaworld",
+    "img" => "img/media/tay-k.jpeg",
+    "genre" => "Hip-hop",
+    "format" => "mp3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Tay-k"
+];
+ ?>
---
+++ MyonlineLibrary/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+  <div class="wrapper">
+    <ul>
+      <li><a href="https://www.instagram.com/laguardiacc/">Instagram</a></li>
+           <li><a href="https://www.facebook.com/laguardiacc/">Facebook</a></li>
+       </ul>
+       <p> &copy; <?php echo date("Y"); ?> Personal Media Library </p>
+
+       </div>
+     </div>
+   </body>
+   </html>
